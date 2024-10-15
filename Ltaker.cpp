cd771181705c4faadb7e54d0ecad80c94eeeb4a8
tsun <95903974+nmt7@users.noreply.github.com>
2024-10-15 14:41:28 +0700

    Update Ltaker.cpp

--- Ltaker.cpp
+++ Ltaker.cpp
@@ -2,20 +2,30 @@
 #include<math.h>
 using namespace std;
 
+// this code splits into 2 sections
+
+// **    <--- square
+// **
+
+// ****  <--- rectangle
+// ****
+
 void LSqr(int sok){
     cout << endl << "hinh vuong khuyet: " << endl;
-    int z = pow(2,sok)/2;
-    int z1 = z*2;
-    // while (chieudaix>0){
-    //     cout << "L";
-    //     chieudaix--;
-    // }
+    
+    int z = pow(2,sok)/2; // length of the first sections/square
+    int z1 = z*2; // length of the second section/rectangle
+
+
+    // one quater of the big square/small square
     for (int x=0; x<z;x++){
         for (int y=0; y<z;y++){
             cout << "L";
         }
         cout << endl;
     }
+
+    // last half of the square/rectangle
     for (int x=0; x<z;x++){
         for (int y=0; y<z1;y++){
             cout << "L";
